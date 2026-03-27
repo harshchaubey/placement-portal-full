b8a264beed232bb50c412ab825dbbf4c48b44e81
harshchaubey <harshchaubey189@gmail.com>
2026-03-27 20:15:06 +0530

    Add Cloudinary configuration class

--- backend/src/main/java/com/placement/portal/CloudinaryConfig.java
+++ backend/src/main/java/com/placement/portal/CloudinaryConfig.java
@@ -1,3 +1,10 @@
+package com.placement.portal;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CloudinaryConfig {
 
